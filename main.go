8125b00b3090d7f5d744baeaf02e12b23c1bbaf9
DaviTostes <davisiqueira591@gmail.com>
2024-10-04 00:55:59 -0300

    feat: init

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/gorilla/websocket"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use:   "grownups",
+		Short: "A live chat private for everyone",
+		Long:  "Dont tread on me",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) < 1 || args[0] == "" {
+				fmt.Println("Usage: grownups <username>")
+				os.Exit(1)
+			}
+
+			run(args[0])
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func run(username string) {
+	url := "wss://grownups-server.onrender.com/ws"
+
+	if username == "" {
+		fmt.Println("Usage: grownups <username>")
+	}
+
+	fmt.Println("Connecting to server...")
+
+	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Send username to server
+	err = conn.WriteMessage(websocket.TextMessage, []byte(username))
+	if err != nil {
+		fmt.Println("Error sending username:", err)
+		return
+	}
+
+	fmt.Println("Connect as " + username)
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	go readMessages(conn)
+
+	for {
+		sendMessage(scanner, conn)
+	}
+}
+
+func readMessages(conn *websocket.Conn) {
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Error reading message:", err)
+			return
+		}
+
+		fmt.Print("\r\033[K")
+		fmt.Println(string(msg))
+		fmt.Print("You: ")
+	}
+}
+
+func sendMessage(scanner *bufio.Scanner, conn *websocket.Conn) {
+	fmt.Print("You: ")
+	if scanner.Scan() {
+		text := scanner.Text()
+		if err := conn.WriteMessage(websocket.TextMessage, []byte(text)); err != nil {
+			fmt.Println("Error sending message:", err)
+			return
+		}
+	}
+}
